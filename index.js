fc269302cea8f5397065f93d4dfb9c2bc9c6714f
KamaUnik <6638012721@student.chula.ac.th>
2025-10-03 16:35:14 +0700

    Tried Added some cols

--- index.js
+++ index.js
@@ -7,16 +7,24 @@ const app = express();
 const server = createServer(app);
 const io = new Server(server);
 
+
 app.get('/', (req, res) => {
   res.sendFile(join(__dirname, 'index.html'));
 });
 
 io.on('connection', (socket) => {
+  const id = socket.id;
+  console.log(id+" has connected");
   socket.on('chat message', (msg) => {
-    io.emit('chat message', msg);
+    const text = id + " : " + msg
+    io.emit('chat message', (text));
   });
 });
 
 server.listen(3000, () => {
   console.log('server running at http://localhost:3000');
 });
+
+//Fundmental Checkpoint close to finish
+//Can do multiple devices
+//As of 9/19 10%
