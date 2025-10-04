27c1558117455dbc99ad08adafa488d1c1faada7
Chopin <chopin.won@gmail.com>
2025-10-04 11:40:07 +0700

    Username works better now.

--- index.js
+++ index.js
@@ -14,14 +14,20 @@ app.get('/', (req, res) => {
 
 io.on('connection', (socket) => {
   const id = socket.id;
+  let user = { "id": id, "name": "Unnamed"};
   //const joinNoti = id + " has joined."
   console.log(id+" is cool.");
   socket.on('chat message', (msg) => {
-    const text = `${id}: ${msg}`
+    const text = `${user.name}: ${msg}`
     io.emit('chat message', (text));
   });
   socket.on('status message',(msg) => {
-    io.emit('status message',msg);
+    const text = `Username is now ${msg}!`
+    user.name = msg;
+    io.emit('status message',text);
+  })
+  socket.on('set name',(name)=>{
+    user.name = name;
   })
 });
 
